422c9485ae07bb2c3a67f1a19ad5f84165d89b1d
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-08-18 18:54:28 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,3 +1,16 @@
+function getAjax(url, success) {
+  var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+  xhr.open("GET", url);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState > 3 && xhr.status === 200) success(xhr.responseText);
+  };
+  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  xhr.send();
+  return xhr;
+}
 (function(){
   console.log("Database Installed!");
+  getAjax("https://Dragon-Database.simplexshotz.repl.run/?a=1", function(data) {
+    console.log(data);
+  });
 })();
